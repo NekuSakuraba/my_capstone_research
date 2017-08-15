4a58fa04d42ffd72d3c0bb656aa54149833090ec
Rodrigo Miike <drimiksi@gmail.com>
2017-08-15 16:30:16 -0300

    multivariate t

--- subjects/em/multivariate_util.py
+++ subjects/em/multivariate_util.py
@@ -1,8 +1,26 @@
 # Source
 ## https://github.com/statsmodels/statsmodels/blob/master/statsmodels/sandbox/distributions/multivariate.py#L90
 import numpy as np
-from scipy.special import gamma
+from numpy import log
+
+from scipy.special import gamma, digamma
 from scipy.linalg import inv, det
+
+from sklearn.cluster import KMeans
+
+def get_random(X):
+    """Get a random sample from X.
+    Parameters
+    ----------
+    X: array-like, shape (n_samples, n_features)
+    
+    Returns
+    -------
+    array-like, shape (1, n_features)
+    """
+    size = len(X)
+    idx = np.random.choice(range(size))
+    return X[idx]
 
 #written by Enzo Michelangeli, style changes by josef-pktd
 # Student's T random variable
@@ -76,3 +94,125 @@ class multivariate_t:
     
     def __repr__(self):
         return 'mu: %s;\n df: %s;\n sigma: %s' % (self.mu, self.df, self.sigma)
+
+class MultivariateTMixture:
+    """
+    Parameters
+    ----------
+    n_components: number of components
+    """
+    def __init__(self, n_components):
+        self.n_components = n_components
+    
+    def fit(self, X):
+        self.p = X.shape[1]
+        
+        clf = KMeans(n_clusters=self.p)
+        clf.fit(X)
+        
+        xmin = get_random(X)
+        xmax = get_random(X)
+        xcov = np.cov(X.T.copy())
+        self.mixes = [multivariate_t(mu=clf.cluster_centers_[_], sigma=xcov, df=4) for _ in range(self.n_components)]
+        self.pi    = [1./self.n_components for _ in range(self.n_components)]
+    
+    def __calc_tau(self, X):
+        # E-step: Calculating tau
+        self.weights = {}
+        for idx in range(self.n_components):
+            self.weights[idx] = (self.mixes[idx].pdf(X) * self.pi[idx]).reshape(-1, 1)
+        weights_total = sum(self.weights.values())
+        self.weights_total = weights_total
+        
+        # normalizing weights
+        for idx, _ in enumerate(self.weights):
+            self.weights[idx] /= weights_total
+    
+    def __calc_u(self, X):
+        # E-Step: Calculating u
+        self.u = {}
+        for idx in range(self.n_components):
+            _ = []
+            inv_sigma = inv(self.mixes[idx].sigma)
+            for delta in (X-self.mixes[idx].mu):
+                _.append(delta.dot(inv_sigma).dot(delta))
+            _ = np.array(_)
+            self.u[idx] = (self.mixes[idx].df + self.p)/(self.mixes[idx].df + _)
+            self.u[idx] = self.u[idx].reshape(-1, 1)
+            
+    def __calc_u2(self, X):
+        # E-Step: Calculating u
+        self.u = {}
+        for idx in range(self.n_components):
+            _ = []
+            for delta in (X-self.mixes[idx].mu):
+                _.append(delta.dot(inv(self.mixes[idx].sigma)).dot(delta))
+            _ = np.array(_)
+            self.u[idx] = (self.mixes[idx].df + self.p)/(self.mixes[idx].df + _)
+            self.u[idx] = self.u[idx].reshape(-1, 1)
+    
+    def e_step1(self, X):
+        self.__calc_tau(X)
+        self.__calc_u(X)
+    
+    def e_step2(self, X):
+        self.__calc_u(X)
+
+    def cm_step1(self, X):
+        for idx, _ in enumerate(self.mixes):
+            # Assigning mu and cov
+            self.mixes[idx].mu, self.mixes[idx].sigma = self.__estimate_parameters(X, self.u[idx], self.weights[idx])
+
+    def __delta(self, X, mu):
+        for idx, _ in enumerate(X - mu):
+            yield idx, _.reshape(-1, 1)
+
+    def __estimate_parameters(self, X, u, tau):
+        tau_u = tau * u
+        mu_ = (tau_u * X).sum(axis=0) / tau_u.sum() # Here I could improve somehow...
+        
+        cov_ = np.array([[0,0], [0,0]], dtype=np.float32)
+        for idx, delta in self.__delta(X, mu_):
+            #delta = delta.reshape(-1, 1)
+            cov_ += (tau[idx] * u[idx] * delta).dot(delta.T)
+        
+        cov_ /= tau.sum()
+        
+        return mu_, cov_
+
+    def __estimate_dof(self, v, u, tau):
+        return -digamma(v/2.) + log(v/2.) + (tau * (log(u) - u)).sum()/tau.sum() + 1 + (digamma((v+self.p)/2.)-log((v+self.p)/2.))
+    
+    def cm_step2(self, X):
+        for idx, _ in enumerate(self.mixes):
+            arange = np.arange(_.df, _.df+3, .01)
+            for df in arange:
+                solution = self.__estimate_dof(df, self.u[idx], self.weights[idx])
+                if solution < 0+1e-4 and solution > 0-1e-4:
+                    # Assigning degrees of freedom
+                    _.df = df
+                    break
+
+        for idx, pi in enumerate(self.pi):
+            self.pi[idx] = sum(self.weights[idx])/len(self.weights[idx])
+    
+    def pdf(self, X):
+        result = []
+        for idx, mix in enumerate(self.mixes):
+            result.append(mix.pdf(X) * self.pi)
+
+    def log_likelihood(self, X):
+        likelihood = 0
+        for _ in self.pi:
+            likelihood += len(X) * log(_)
+        
+        for mix in self.mixes:
+            likelihood += log(mix.pdf(X)).sum()
+        
+        return likelihood
+    
+    def iterate(self, X):
+        self.e_step1(X)
+        self.cm_step1(X)
+        self.e_step2(X)
+        self.cm_step2(X)
